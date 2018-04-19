62ed60fb6086b5eade3594d94d67e6baf6097483
ysoftman <ysoftman@gmail.com>
2018-04-19 11:39:32 +0900

    미세먼지,초미세먼지 WHO 등급으로 판단

--- watchDust.go
+++ watchDust.go
@@ -177,12 +177,61 @@ func analDustInfo(jsonDustInfo *dustinfoResp) {
 		"일산화탄소: " + jsonDustInfo.List[i].CoValue + "(" + toGradeStr(jsonDustInfo.List[i].CoGrade) + ")" + "\n" +
 		"오존: " + jsonDustInfo.List[i].O3Value + "(" + toGradeStr(jsonDustInfo.List[i].O3Grade) + ")" + "\n" +
 		"이산화질소: " + jsonDustInfo.List[i].No2Value + "(" + toGradeStr(jsonDustInfo.List[i].No2Grade) + ")" + "\n" +
-		"미세먼지(pm10): " + jsonDustInfo.List[i].Pm10Value + "(" + toGradeStr(jsonDustInfo.List[i].Pm10Grade) + ")" + "\n" +
-		"초미세먼지(pm25): " + jsonDustInfo.List[i].Pm25Value + "(" + toGradeStr(jsonDustInfo.List[i].Pm25Grade) + ")" + "\n"
-
+		// "미세먼지(pm10): " + jsonDustInfo.List[i].Pm10Value + "(" + toGradeStr(jsonDustInfo.List[i].Pm10Grade) + ")" + "\n" +
+		// "초미세먼지(pm25): " + jsonDustInfo.List[i].Pm25Value + "(" + toGradeStr(jsonDustInfo.List[i].Pm25Grade) + ")" + "\n"
+		"미세먼지(pm10): " + jsonDustInfo.List[i].Pm10Value + "(" + toWHOPM10GradeStr(jsonDustInfo.List[i].Pm10Value) + ")" + "\n" +
+		"초미세먼지(pm25): " + jsonDustInfo.List[i].Pm25Value + "(" + toWHOPM25GradeStr(jsonDustInfo.List[i].Pm25Value) + ")" + "\n"
 	log.Println(dustinfoMsg)
 
 	sendToSlack(dustinfoMsg)
+}
+
+func toWHOPM10GradeStr(value string) string {
+	nValue, err := strconv.Atoi(value)
+	if err == nil {
+		log.Println(err)
+		return "_"
+	}
+	if nValue <= 15 {
+		return "최고:shuffle_parrot:"
+	} else if nValue <= 30 {
+		return "좋음:party_parrot:"
+	} else if nValue <= 40 {
+		return "양호:pikachu:"
+	} else if nValue <= 50 {
+		return "보통:smile:"
+	} else if nValue <= 75 {
+		return "나쁨:scream:"
+	} else if nValue <= 100 {
+		return "상당히나쁨:angry:"
+	} else if nValue <= 150 {
+		return "매우나쁨:angryy:"
+	}
+	return "최악:angry:"
+}
+
+func toWHOPM25GradeStr(value string) string {
+	nValue, err := strconv.Atoi(value)
+	if err == nil {
+		log.Println(err)
+		return "_"
+	}
+	if nValue <= 8 {
+		return "최고:shuffle_parrot:"
+	} else if nValue <= 15 {
+		return "좋음:party_parrot:"
+	} else if nValue <= 20 {
+		return "양호:pikachu:"
+	} else if nValue <= 25 {
+		return "보통:smile:"
+	} else if nValue <= 37 {
+		return "나쁨:scream:"
+	} else if nValue <= 50 {
+		return "상당히나쁨:angry:"
+	} else if nValue <= 75 {
+		return "매우나쁨:angryy:"
+	}
+	return "최악:angry:"
 }
 
 func toGradeStr(grade string) string {
@@ -191,9 +240,9 @@ func toGradeStr(grade string) string {
 	} else if grade == "2" {
 		return "보통:smile:"
 	} else if grade == "3" {
-		return "나쁨:angry:"
+		return "나쁨:scream:"
 	} else if grade == "4" {
-		return "매우나쁨:angryyy:"
+		return "매우나쁨:angryy:"
 	}
 	return "_"
 }
